561a00c25415e1902b423668c3abadb3d00705fa
Lem4aks <Lem4aks@gmail.com>
2024-06-20 23:32:04 +0300

    вход и регистрация

--- RentalFront/Program.cs
+++ RentalFront/Program.cs
@@ -1,5 +1,7 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using static RentalFront.Layout.MainLayout;
 
 namespace RentalFront
 {
@@ -11,6 +13,7 @@ namespace RentalFront
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7029/") });
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }
